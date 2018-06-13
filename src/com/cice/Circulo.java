9834a703cfa5d5a93d2c5fffc187e5042248c1c0
cice <cice@A8POVMac10.local>
2018-06-13 21:24:42 +0200

    Ejercicio circulo mod

---
+++ src/com/cice/Circulo.java
@@ -0,0 +1,54 @@
+import static java.lang.Math.*;
+
+public class Circulo {
+
+
+    private double area;
+    private double perimetro;
+    private double radio;
+
+
+
+
+    public Circulo(double rad){
+
+        radio=rad;
+    }
+
+    public double calculaArea(){
+
+        area=0;
+        area =radio*radio* PI;
+        return area;
+    }
+
+    public double calcularPerimetro(){
+
+        perimetro=2*radio* Math.PI;
+        return perimetro;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public double getPerimetro() {
+        return perimetro;
+    }
+
+    public void setPerimetro(double perimetro) {
+        this.perimetro = perimetro;
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+}
